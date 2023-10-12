54b51547e5b4d1631da9268b486807352fb686c7
tushar4g <tushar4sahu@gmail.com>
2023-10-12 16:19:22 +0530

    write javascript for btn but not added this script is in body section of index.html

---
+++ index.js
@@ -0,0 +1,7 @@
+const mobile_nav = document.querySelector(".navbar-mobile-btn");
+
+const toggleNavbar = () => {
+    alert("tushar clicked");
+}
+
+mobile_nav.addEventListener('click', () => toggleNavbar());
